d919fa66dceb41e2a925b48497051f393f71cfcd
Gautam Mittal <200mittalgauatam@gmail.com>
2018-12-05 19:04:17 +0530

    added middleware

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var morgan = require('morgan')
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config');
 
-var app  = express()
+var app  = express();
 
-app.listen(3000, function(err) {
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+
+app.listen(config.port, function(err) {
 	if(err) {
 	console.log(err)
 	} else {
 	console.log("listening on port 3000")
 }
 
-})
+});
