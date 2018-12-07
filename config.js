b6518bd14d52047c3e6d3234d7afcd8372fddc6e
Gautam Mittal <200mittalgautam@gmail.com>
2018-12-07 18:55:23 +0530

    made changes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"database": "mongodb://<dbuser>:<dbpassword>@ds127944.mlab.com:27944/userstory",
+	"database": "mongodb://root:abc123@ds127944.mlab.com:27944/userstory",
 	"port": 3000,
 	"secretKey": "TobeDefined"
 }
